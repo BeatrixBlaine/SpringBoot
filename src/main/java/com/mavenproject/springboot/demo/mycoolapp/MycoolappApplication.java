65b3c8e70de838d1b5f5be0141ba909354b7ce10
Icad <beatrixblaine@gmail.com>
2026-08-10 13:43:17 +0700

    Setup mySql driver & Spring Data JPA, create CommandLineRunner Bean on main app class

--- src/main/java/com/mavenproject/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/mavenproject/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -1,8 +1,10 @@
 package com.mavenproject.springboot.demo.mycoolapp;
 
 import com.mavenproject.springboot.demo.mycoolapp.rest.FunRestController;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(
 		// scanning all packages
@@ -12,6 +14,13 @@ public class MycoolappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MycoolappApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hello World!");
+		};
 	}
 
 }
