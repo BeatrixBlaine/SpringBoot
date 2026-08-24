8aefd3a5cfc36b5c02df05fa92338517d56e6b43
Icad <beatrixblaine@gmail.com>
2026-08-24 10:51:43 +0700

    adding new endpoints, PATCH with JsonMapper

--- src/main/java/com/mavenproject/springboot/demo/mycoolapp/rest/EmployeeController.java
+++ src/main/java/com/mavenproject/springboot/demo/mycoolapp/rest/EmployeeController.java
@@ -2,18 +2,24 @@ package com.mavenproject.springboot.demo.mycoolapp.rest;
 
 import com.mavenproject.springboot.demo.mycoolapp.entity.Employee;
 import com.mavenproject.springboot.demo.mycoolapp.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import tools.jackson.databind.json.JsonMapper;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
+    private final JsonMapper jsonMapper;
 
-    public EmployeeController(EmployeeService employeeService) {
+    @Autowired
+    public EmployeeController(EmployeeService employeeService, JsonMapper jsonMapper) {
         this.employeeService = employeeService;
+        this.jsonMapper = jsonMapper;
     }
 
     // return list of employees
@@ -52,5 +58,39 @@ public class EmployeeController {
         return employeeService.save(theEmployee);
     }
 
+    // patch employee
+    @PatchMapping("/employees/{employeeId}")
+    public Employee patchEmployee(@PathVariable int employeeId, @RequestBody Map<String, Object> patchPayload) {
+
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        // throw exception if null
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee Id not found - " + employeeId);
+        }
+
+        // throw exception if request body contains "id" key
+        if (patchPayload.containsKey("id")) {
+            throw new RuntimeException("Employee Id not allowed in request body - " + employeeId);
+        }
+
+        Employee patchedEmployee = jsonMapper.updateValue(tempEmployee, patchPayload);
+
+        return employeeService.save(patchedEmployee);
+    }
+
+
     // delete employee
+    @DeleteMapping("/employees/{employeeId}")
+    public Employee deleteEmployee(@PathVariable int employeeId) {
+
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee == null) {
+            throw new RuntimeException("Employee Id not found - " + employeeId);
+        }
+        employeeService.deleteById(employeeId);
+
+        return theEmployee;
+    }
 }
