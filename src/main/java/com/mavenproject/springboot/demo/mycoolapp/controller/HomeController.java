4ae16e5a28a18f814032702eaf805dadbf09d9f2
Icad <beatrixblaine@gmail.com>
2026-09-05 18:04:20 +0700

    testing form input, render it in another page

--- src/main/java/com/mavenproject/springboot/demo/mycoolapp/controller/HomeController.java
+++ src/main/java/com/mavenproject/springboot/demo/mycoolapp/controller/HomeController.java
@@ -3,17 +3,32 @@ package com.mavenproject.springboot.demo.mycoolapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
     // create mapping for helloworld
-    @GetMapping("/home")
-    public String home(Model model) {
+    @GetMapping("/hello")
+    public String sayHello(Model model) {
 
         model.addAttribute("TheDate", java.time.LocalDateTime.now());
 
         // automatically look return "helloworld.html"
         return "helloworld";
     }
+
+    // method to show initial HTML form
+    @GetMapping("/showForm")
+    public String showForm() {
+        return "helloworld-form";
+    }
+
+    // method to process the HTML form
+    @GetMapping("/processForm")
+    public String processForm() {
+        return "process-form";
+    }
+
 }
